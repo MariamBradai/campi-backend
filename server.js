9e13eb3b3f2a9929af0fe2154b03c7ef66d5bfc0
Mariam Bradai <mariambradai2@gmail.com>
2022-03-27 20:06:32 +0100

    Set up cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express()
-
+app.use(cors({ credentials: true, origin: [process.env.WEB_APP_URL] }))
 app.use(express.json())
 
 let countries = [
